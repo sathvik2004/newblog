5b25dfdbd57a0ec83ed6095ce8a04e85e794e8cc
sathvik2004 <joshisyteeg@gmail.com>
2024-04-19 22:22:19 +0530

    Updates

---
+++ precache-manifest.a6b0cb890179233246173a12304ed15b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3031565a21077ed3a375",
+    "url": "/newblog/static/css/main.58ef478c.chunk.css"
+  },
+  {
+    "revision": "3031565a21077ed3a375",
+    "url": "/newblog/static/js/main.3031565a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/newblog/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "f848d690f540de6ad400",
+    "url": "/newblog/static/js/runtime~main.f848d690.js"
+  },
+  {
+    "revision": "b16a22db4f6135c8e066a7f5e9c3176c",
+    "url": "/newblog/index.html"
+  }
+];
